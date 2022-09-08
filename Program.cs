8906845076cead822c38f26ef3ee18abe065c708
PyXleb <76917344+PyXleb@users.noreply.github.com>
2022-09-08 18:35:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+class launcher{
+	public static void Main(){
+		var wnd = new Process{StartInfo = new ProcessStartInfo{FileName = "cmd.exe", RedirectStandardInput = true, UseShellExecute = false}};
+		wnd.Start();
+		using(StreamWriter stdo = wnd.StandardInput) stdo.WriteLine("start \"\" \"%cd%/index.html\"");
+		Console.Write("\n\nFTKLauncher, build10000\nPyXleb, 2022\n\nИсходники доступны по ссылке в readme!\n\nНажмите любую клавишу, чтобы закрыть окно...\n");
+		Console.ReadKey();
+	}
+}
